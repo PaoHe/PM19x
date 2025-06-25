ea657abf29038df1beb076a3d06aea909f3a47f9
Paohdez <122041731@upq.edu.mx>
2025-06-24 23:02:36 -0600

    Practica No.10: ImageBackgroung & SlapshScreen

--- my-blank/App.js
+++ my-blank/App.js
@@ -1,98 +1,58 @@
-//Zona 1: importaciones 
-
-import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
-import { StyleSheet, Text, View, Switch } from 'react-native';
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+/* Zona 1: Importaciones */
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, Alert, StyleSheet } from 'react-native';
 
 
-//Zona 2: Main 
-
+/* Zona 2: Main(ejecutacion) */
 export default function App() {
+  const [nombre, setNombre] = useState('');
 
-const[activarSwitch, setActivarSwich] = useState (false);
-const[modoOscuro, setModoOscuro] = useState (false);
+  const mostrarAlerta = () => {
+    if (nombre.trim() === '') {
+      Alert.alert('Error', 'Por favor escribe algo');
+      alert('Escribe algo jilipollas');
+    } else {
+      Alert.alert('Bienvenida', `Hola ${nombre}, bienvenido a nuestra app :D`);
+      alert('Hola ' + nombre + ' bienvenid@ a nuestra app :D');
+
+    }
+  };
 
   return (
-    <SafeAreaProvider>
-      <SafeAreaView style = {[styles.contenedor, modoOscuro && styles.fondoOscuro ]}>
+    <View style={styles.container}>
+      <Text style={styles.text}>Ingresa tu nombre:</Text>
 
-        {/*Aqui van los Componentes*/}
-        <Text style = {[styles.titulo, modoOscuro && styles.textoClaro]}>
-          Practica con Switchs 
-        </Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Escribe tu nombre :"
+        onChangeText={setNombre}
+        value={nombre}
+      />
 
-        <View style={[styles.opcion]}>
-          <Text style={[styles.etiqueta, modoOscuro && styles.textoClaro]}> 
-            Activar Switch 2
-          </Text>
-
-          <Switch 
-          value={activarSwitch}
-          onValueChange={setActivarSwich}
-          trackColor={{false:'#ccc',true: '#4caf50'}}
-          thumbColor={activarSwitch? '#ffffff': '#99999'}>
-          </Switch>
-        </View>
-
-        <View style={[styles.opcion]}>
-          <Text style={[styles.etiqueta, modoOscuro && styles.textoClaro]}> 
-            Modo Oscuro 
-          </Text>
-
-          <Switch 
-          value={modoOscuro}
-          onValueChange={setModoOscuro}
-          disabled={activarSwitch}
-          trackColor={
-            activarSwitch
-            ? {false: '#ff9999', true: '#ff3b30'}
-              :{false: ' #cc', true: '#4caf50'}
-            }
-          thumbColor={
-            activarSwitch
-          ? '#ff3b30'
-          : modoOscuro
-          ?'#ffffff'
-          :'#999999'
-        }>
-          </Switch>
-        </View>
-
-      </SafeAreaView>
-    </SafeAreaProvider>
-
+      <Button title="Enviar" onPress={mostrarAlerta} />
+    </View>
   );
 }
 
-//Zona 3: Estilos 
-
 const styles = StyleSheet.create({
-  contenedor: {
+  container: {
     flex: 1,
-    backgroundColor: '#fff',
-    paddingHorizontal: 30,
     justifyContent: 'center',
+    padding: 20,
+    backgroundColor: 'white', 
   },
-  titulo:{
-    fontSize: 24,
-    marginBlock: 40,
-    texteAlign: 'center',
-    fontWeight: 'boid',
-  },
-  fondoOscuro:{
-    backgroundColor: "#1a1a1a",
-  },
-  textoClaro: {
-    color: '#ffffff',
-  },
-  opcion:{
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginBottom: '30',
-    alignItems: 'center', 
-  },
-  etiqueta:{
+  text: {
     fontSize: 18,
-  }
+    marginBottom: 10,
+    color: '#000', 
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc', 
+    padding: 10,
+    marginBottom: 20,
+    borderRadius: 5,
+    backgroundColor: '#f9f9f9', 
+    color: '#000', 
+  },
 });
